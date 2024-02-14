8b98d10dcb564913e76b03f9aa6899c18e011111
Matheus <meduardo.valentim@gmail.com>
2024-02-14 15:25:38 -0300

    feat: created types for styles

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,15 @@
+export const defaultTheme = {
+    'primary-000':'#E6CC00',
+    'primary-100':'#D4AF37',
+    'primary-200':'#b49214',
+    'primary-300':'#6c5400',
+    'accent-100':'#C0C0C0',
+    'accent-200':'#626262',
+    'text-100':'#FFFFFF',
+    'text-200':'#e0e0e0',
+    'bg-100':'#1E1E1E',
+    'bg-200':'#2d2d2d',
+    'bg-300':'#454545',
+      
+  }
+  
