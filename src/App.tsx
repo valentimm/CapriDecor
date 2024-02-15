428e357f18572530a2ac2a11a1e1b597779db93c
Matheus <meduardo.valentim@gmail.com>
2024-02-14 23:43:41 -0300

    feat: create Controle Interno, Dados do Cliente e Ambiente

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,132 @@
 import { ThemeProvider } from "styled-components";
 import { GlobalStyle } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
+import { Header, Main } from "./styles"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
-        <h1>Hello World</h1>
+        <Header>
+          <div id="headerBanner">
+            <img src="/logo.png" alt="logo" />
+          </div>
+        </Header>
+        <Main>
+          <form id="mainForm">
+            <h2>Controle Interno</h2>
+              <form id="internalControl">
+                <div >
+                  <label htmlFor="data">Data:</label>
+                  <input type="Date" id="data" name="name" />
+                </div>
+
+                <div>
+                  <label htmlFor="responsavel">Responsável técnico:</label>
+                  <input type="text" id="responsavel" name="nameTech" />
+                </div>
+
+                <div>
+                  <label htmlFor="observacoes">Observações:</label>
+                  <input type="text" id="observacoes" name="name" />
+                </div>
+                <div>
+                  <input type="checkbox" id="orçamento" name="orçamento" value="Orçamento" />
+                  <label htmlFor="orçamento"> Orçamento</label>
+                  <input type="checkbox" id="pedido" name="pedido" value="Pedido" />
+                  <label htmlFor="pedido"> Pedido</label>
+                </div>
+                <div >
+                  <label htmlFor="data">Data da confirmação:</label>
+                  <input type="Date" id="data" name="name" />
+                </div>
+              </form>
+            <h2>Dados do Cliente</h2>
+              <form id="clientInfo">
+                <div>
+                  <label htmlFor="name">Nome: </label>
+                  <input type="text" id="responsavel" name="Nome" />
+                </div>
+                <div>
+                  <label htmlFor="cpf">CPF/CNPJ: </label>
+                  <input type="number" id="cpf" name="Documento" />
+                </div>
+                <div>
+                  <label htmlFor="cep">CEP: </label>
+                  <input type="number" id="cep" name="CEP" />
+                </div>
+                <div>
+                  <label htmlFor="endereco">Endereço: </label>
+                  <input type="text" id="endereco" name="Endereço" />
+                </div>
+                <div>
+                  <label htmlFor="numero">Número: </label>
+                  <input type="number" id="numero" name="Número" />
+                </div>
+                <div>
+                  <label htmlFor="bairro">Bairro: </label>
+                  <input type="text" id="bairro" name="Bairro" />
+                </div>
+                <div>
+                  <label htmlFor="cidade">Cidade: </label>
+                  <input type="text" id="cidade" name="Cidade" />
+                </div>
+                <div>
+                  <label htmlFor="telefone">Telefone: </label>
+                  <input type="number" id="telefone" name="Telefone" />
+                </div>
+                <div>
+                  <label htmlFor="contato">Contato: </label>
+                  <input type="number" id="contato" name="Contato" />
+                </div>
+                <div>
+                  <label htmlFor="email">Email: </label>
+                  <input type="email" id="email" name="E-mail" />
+                </div>
+                <div>
+                  <label htmlFor="observacoes">Como chegou ate a CapriDecor:</label>
+                  <input type="checkbox" id="indicação" name="indicação" value="indicação" />
+                  <label htmlFor="indicação"> Indicação</label>
+                  <input type="checkbox" id="instagram" name="instagram" value="instagram" />
+                  <label htmlFor="instagram"> Instagram</label>
+                  <input type="checkbox" id="site" name="site" value="site" />
+                  <label htmlFor="site"> Site</label>
+                </div>
+                <div >
+                  <label htmlFor="data">Data da confirmação:</label>
+                  <input type="Date" id="data" name="name" />
+                </div>
+              </form>
+            <h2>Ambiente</h2>
+              <form id="ambient">
+                <div>
+                    <label htmlFor="ambiente01"></label>
+                    <input type="text" id="ambiente01" name="Ambiente01" />
+                    <label htmlFor="altura01">Altura</label>
+                    <input type="number" id="altura01" name="AlturaAmbiente01" />
+                    <label htmlFor="largura01">Largura</label>
+                    <input type="number" id="largura01" name="LarguraAmbiente01" />
+                </div>
+                <div>
+                    <label htmlFor="ambiente02"></label>
+                    <input type="text" id="ambiente02" name="Ambiente02" />
+                    <label htmlFor="altura02">Altura</label>
+                    <input type="number" id="altura02" name="AlturaAmbiente02" />
+                    <label htmlFor="largura02">Largura</label>
+                    <input type="number" id="largura02" name="LarguraAmbiente02" />
+                </div>
+                <div>
+                    <label htmlFor="ambiente03"></label>
+                    <input type="text" id="ambiente03" name="Ambiente03" />
+                    <label htmlFor="altura03">Altura</label>
+                    <input type="number" id="altura03" name="AlturaAmbiente03" />
+                    <label htmlFor="largura03">Largura</label>
+                    <input type="number" id="largura03" name="LarguraAmbiente03" />
+                </div>
+              </form>
+
+        </form>
+        </Main>
     </ThemeProvider>
   )
 }
---
+++ src/styles.ts
@@ -0,0 +1,153 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    #headerBanner {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 2rem 0;
+        //border-bottom: 1px solid #eaeaea;
+
+        img {
+            width: auto;
+            height: auto;
+        }
+    }
+
+  @media (max-width: 768px) {
+
+    #headerBanner {
+    padding-top: 2rem;
+      img {
+        width: 50%;
+        height: 50%;
+      }
+    }
+  }
+`;
+
+export const Main = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem 0;
+    text-align: center;
+    border: 1px solid red;
+
+    h2 {
+        margin-top: 0;
+        line-height: 1.15;
+        font-size: 1.25rem;
+        font-weight: 200;
+        color: white;
+        border-bottom: 1px solid #ffffff;
+    }
+
+    #mainForm {
+        width: 80%;
+        margin: auto;
+        border: 1px solid green;
+    }
+
+
+    #internalControl {
+        padding: 1rem;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #internalControl div {
+        margin-bottom: 1rem;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    #internalControl label {
+        margin-right: 0.5rem;
+        color: white;
+    }
+
+
+    #internalControl input, select, textarea {
+        width: 100%;
+        padding: 0.5rem;
+        box-sizing: border-box;
+    }
+
+    //----------------------Client Info-----------------------//
+
+    #clientInfo {
+        padding: 1rem;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #clientInfo div {
+        margin-bottom: 1rem;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    #clientInfo label {
+        margin-right: 0.5rem;
+        color: white;
+    }
+
+    #clientInfo input, select, textarea {
+        width: 100%;
+        padding: 0.5rem;
+        box-sizing: border-box;
+    }
+
+    #clientInfo input[type="number"] {
+        width: 100%;
+        padding: 0.5rem;
+        box-sizing: border-box;
+    }
+
+    //----------------------Ambient-----------------------//
+
+    #ambient {
+        padding: 1rem;
+        display: flex;
+        flex-direction: column;
+    }
+
+    #ambient div {
+        margin-bottom: 1rem;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+
+    #ambient label {
+        margin-right: 0;
+        color: white;
+        padding: 0 0.5rem;
+    }
+
+    #ambient input, select, textarea {
+        width: 100%;
+        padding: 0.5rem;
+        box-sizing: border-box;
+    }
+
+
+`;
+
+export const Footer = styled.footer`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 0;
+    border-top: 1px solid #eaeaea;
+    background-color: #f9f9f9;
+    color: #333;
+
+    p {
+        font-size: 0.8rem;
+    }
+`;
