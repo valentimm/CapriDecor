bd8dbd048e9709553bae8e85c5dc7d62eb9d6a83
Matheus <meduardo.valentim@gmail.com>
2024-02-14 22:17:32 -0300

    feat: add font to global

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,7 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Poppins', sans-serif;
+    font-family: 'Raleway', sans-serif;
   }
   body {
     background-color: ${props => props.theme['bg-100']};
